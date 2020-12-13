834df00272448ea312d83a3f42d62e9194f4ae42
Henrik Heynckes <44033091+Spirit344@users.noreply.github.com>
2020-12-13 19:21:16 +0100

    Ploom Scripts C# Stand 13.12.20

---
+++ FloorInputs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorInputs : MonoBehaviour
+{
+    public InputField breite;
+    public InputField laenge;
+    public float fbreite, flaenge, fflaeche;
+
+    public void SetFloorBreite()
+    {
+        fbreite = float.Parse(breite.text);
+    }
+    public void SetFloorLaenge()
+    {
+        flaenge = float.Parse(laenge.text);
+    }
+    public void SetFloorFlaeche()
+    {
+        fflaeche = fbreite * flaenge;
+    }
+    public void SavePlayer()
+    {
+        GlobalControl.Instance.raumbreite = fbreite;
+        GlobalControl.Instance.raumlaenge = flaenge;
+        GlobalControl.Instance.raumflaeche = fflaeche;
+    }
+}
---
+++ ObjectInputs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInputs : MonoBehaviour
+{
+    public InputField breite1;
+    public InputField laenge1;
+    public InputField abstand1;
+    public float obreite, olaenge, oflaeche, oabstand;
+
+    public void SetObjektBreite()
+    {
+        obreite = float.Parse(breite1.text) / 100;
+    }
+    public void SetObjektLaenge()
+    {
+        olaenge = float.Parse(laenge1.text) / 100;
+    }
+    public void SetObjektAbstand()
+    {
+        oabstand = float.Parse(abstand1.text) / 100;
+    }
+    public void SetObjektFlaeche()
+    {
+        oflaeche = obreite * olaenge;
+    }
+    public void SavePlayer()
+    {
+        GlobalControl.Instance.objektbreite = obreite;
+        GlobalControl.Instance.objektlaenge = olaenge;
+        GlobalControl.Instance.objektabstand = oabstand;
+        GlobalControl.Instance.objektflaeche = oflaeche;
+    }
+}
